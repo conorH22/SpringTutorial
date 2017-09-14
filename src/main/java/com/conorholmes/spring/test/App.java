525af1ffc95696e215d28638946d7f58c31ff1ce
conorH22 <lackamoredude@gmail.com>
2017-09-14 19:24:14 +0100

    Upload of spring tutorial project files

---
+++ src/main/java/com/conorholmes/spring/test/App.java
@@ -0,0 +1,21 @@
+package com.conorholmes.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/conorholmes/spring/test/beans/beans.xml");
+
+		Person person = (Person) context.getBean("person");
+		person.speak();
+		Address address =(Address)context.getBean("address");
+		System.out.println(address);
+		System.out.println(person);
+
+		((ClassPathXmlApplicationContext) context).close();
+
+	}
+
+}
